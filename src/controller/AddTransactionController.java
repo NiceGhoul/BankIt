55d91145178eb41e780cc127cc30f46d84f9e46c
Wilson <163714739+NiceGhoul@users.noreply.github.com>
2024-12-15 01:22:18 +0700

    .

--- src/controller/AddTransactionController.java
+++ src/controller/AddTransactionController.java
@@ -16,7 +16,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Cell;
 import javafx.scene.control.ComboBox;
